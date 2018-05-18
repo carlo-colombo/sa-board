8edb6efbef89b1dd491b30f3a955ea8cf751b89e
Carlo Colombo <carlo.colombo@gmx.com>
2018-05-18 07:22:21 +0100

    adding origin for gh-pages offline

--- src/static/service-worker.js
+++ src/static/service-worker.js
@@ -1,7 +1,11 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('board').then(function(cache) {
-      return cache.addAll(['/app.js', '/app.css', '/'])
+      return cache.addAll(
+        ['/app.js', '/app.css', ''].map(function(path) {
+          return self.location.origin + '/sa-board/' + path
+        })
+      )
     })
   )
 })
